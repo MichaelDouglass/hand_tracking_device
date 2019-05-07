eab39d0754fdd117a94f5e5717295d95e393b9e6
Michael Douglass <msd90@case.edu>
2019-05-06 23:47:39 -0400

    tests gyroscope in code

--- hmm_ardy/hmm_ardy.ino
+++ hmm_ardy/hmm_ardy.ino
@@ -1,6 +1,13 @@
 #include "LedsUI.h"
 #include "Hand.h"
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 LedsUI leds(3);
 
 Hand hand(2.0);
@@ -14,6 +21,16 @@ void setup()
   delay(500);
   leds.dot(); leds.dot();
   Serial.begin(9600);
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1){ leds.dash(); };
+  }
+
+  delay(1000);
+
+  bno.setExtCrystalUse(true);
 }
 
 // For Testing the LED cpp and h
@@ -65,5 +82,24 @@ void loop()
   Serial.print(" || ");
   Serial.println(hand.get_state());
   delay(200);
+}
+*/
+
+// Test the x, y, z Orientation
+/*
+void loop()
+{
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+
+  delay(100);
 }
 */

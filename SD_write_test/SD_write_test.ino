5c90ac1a73f4a66037f81801bd2186264fa90328
Michael Douglass <msd90@case.edu>
2019-05-07 17:33:49 -0400

    tets writing gyro data to SD

--- SD_write_test/SD_write_test.ino
+++ SD_write_test/SD_write_test.ino
@@ -1,5 +1,12 @@
 #include <SD.h>
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 File myFile;
 
 void setup()
@@ -16,26 +23,59 @@ void setup()
     Serial.println("initialization failed!");
     return;
   }
+
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  bno.setExtCrystalUse(true);
+
   Serial.println("initialization done.");
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
 
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-	// close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void loop()
 {
-	// nothing happens after setup
+  myFile = SD.open("gyro.txt", FILE_WRITE);
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  int x = int(event.orientation.x);
+  int y = int(event.orientation.y);
+  int z = int(event.orientation.z);
+  /* Display the floating point data */
+  Serial.print("X: ");
+  Serial.print(x);
+  Serial.print("\tY: ");
+  Serial.print(y);
+  Serial.print("\tZ: ");
+  Serial.print(z);
+  Serial.println("");
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to gyro.txt...");
+    myFile.print(x);
+    myFile.print("\t");
+    myFile.print(y);
+    myFile.print("\t");
+    myFile.print(z);
+    myFile.print("\t");
+    myFile.println(millis());
+  // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening gyro.txt");
+  }
+  delay(750);
 }
